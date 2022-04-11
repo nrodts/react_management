e4f4dbf260bf9c5f938e84e5f1c23fe1536a4be9
nrodts <dl99145440@gmail.com>
2022-04-11 22:22:55 +0900

    Metarial +

--- src/App.js
+++ src/App.js
@@ -1,51 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit *3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
-  'name': 'MoongTangE',
-  'birthday': '991103',
+  'name': 'Lee',
+  'birthday': '0123456',
   'gender': 'Male',
-  'job': 'Feminist'
+  'job': 'Cook'
 },
 {
   'id': 2,
   'image': 'https://placeimg.com/64/64/2',
-  'name': 'Tajiri',
+  'name': 'Kim',
   'birthday': '123455',
   'gender': 'Male',
-  'job': 'Feminists'
+  'job': 'zzxc'
 },
 {
   'id': 3,
   'image': 'https://placeimg.com/64/64/3',
-  'name': 'Kane',
+  'name': 'Dbtest',
   'birthday': '129412',
   'gender': 'Male',
-  'job': 'MTE'
+  'job': 'Db?'
 }]
 
 class App extends Component {
   render() {
     return (
       <div>
-        {
-          customers.map(c => {
-            return (<Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-            );
-          })
-        }
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); } ) }
+          </TableBody>
+        </Table>
       </div>
     );
   }
